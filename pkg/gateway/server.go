451b7282fb0609be1de8606d0e084ae01f38a34c
Ankit Agrawal <ankit14agrawal@gmail.com>
2020-09-30 18:16:57 +0530

    Added few logs

--- pkg/gateway/server.go
+++ pkg/gateway/server.go
@@ -41,14 +41,17 @@ func (*GatewayServerImpl) ActivateJobs(req *pb.ActivateJobsRequest, srv pb.Gatew
 }
 func (*GatewayServerImpl) CancelWorkflowInstance(ctx context.Context, req *pb.CancelWorkflowInstanceRequest) (*pb.CancelWorkflowInstanceResponse, error) {
 	response, err := zbClient.CancelWorkflowInstance(ctx, req)
+	log.Println("Cancelled workflow instance: ", req)
 	return response, err
 }
 func (*GatewayServerImpl) CompleteJob(ctx context.Context, req *pb.CompleteJobRequest) (*pb.CompleteJobResponse, error) {
 	response, err := zbClient.CompleteJob(ctx, req)
+	log.Println("Completed job: ", req)
 	return response, err
 }
 func (*GatewayServerImpl) CreateWorkflowInstance(ctx context.Context, req *pb.CreateWorkflowInstanceRequest) (*pb.CreateWorkflowInstanceResponse, error) {
 	response, err := zbClient.CreateWorkflowInstance(ctx, req)
+	log.Println("Created workflow instance: ", req)
 	return response, err
 }
 func (*GatewayServerImpl) CreateWorkflowInstanceWithResult(ctx context.Context, req *pb.CreateWorkflowInstanceWithResultRequest) (*pb.CreateWorkflowInstanceWithResultResponse, error) {
@@ -57,10 +60,12 @@ func (*GatewayServerImpl) CreateWorkflowInstanceWithResult(ctx context.Context, 
 }
 func (*GatewayServerImpl) DeployWorkflow(ctx context.Context, req *pb.DeployWorkflowRequest) (*pb.DeployWorkflowResponse, error) {
 	response, err := zbClient.DeployWorkflow(ctx, req)
+	log.Println("Deployed workflow.")
 	return response, err
 }
 func (*GatewayServerImpl) FailJob(ctx context.Context, req *pb.FailJobRequest) (*pb.FailJobResponse, error) {
 	response, err := zbClient.FailJob(ctx, req)
+	log.Println("Failed job: ", req)
 	return response, err
 }
 func (*GatewayServerImpl) ThrowError(ctx context.Context, req *pb.ThrowErrorRequest) (*pb.ThrowErrorResponse, error) {
@@ -73,6 +78,7 @@ func (*GatewayServerImpl) PublishMessage(ctx context.Context, req *pb.PublishMes
 }
 func (*GatewayServerImpl) ResolveIncident(ctx context.Context, req *pb.ResolveIncidentRequest) (*pb.ResolveIncidentResponse, error) {
 	response, err := zbClient.ResolveIncident(ctx, req)
+	log.Println("Resolved incident: ", req)
 	return response, err
 }
 func (*GatewayServerImpl) SetVariables(ctx context.Context, req *pb.SetVariablesRequest) (*pb.SetVariablesResponse, error) {
@@ -81,6 +87,7 @@ func (*GatewayServerImpl) SetVariables(ctx context.Context, req *pb.SetVariables
 }
 func (s *GatewayServerImpl) Topology(context.Context, *pb.TopologyRequest) (*pb.TopologyResponse, error) {
 	topology, err := zbClient.Topology(ctx, &pb.TopologyRequest{})
+	log.Println("Fetched topology: ", topology)
 	return topology, err
 }
 func (*GatewayServerImpl) UpdateJobRetries(ctx context.Context, req *pb.UpdateJobRetriesRequest) (*pb.UpdateJobRetriesResponse, error) {
